424c71b39245117d4a3db9bc9d2c84c511e237f2
cys1102 <cys1102@gmail.com>
2026-05-12 20:17:40 +0000

    Deploying to gh-pages from @ yunsungchung/yunsungchung.github.io@bb82aa4fa33346394814fa4e337f8036ee0d1036 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -434,7 +434,7 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/cv_yunsung.pdf", "_blank");
+          window.open("/assets/pdf/cv.pdf", "_blank");
         },
       },{
         id: 'social-email',
